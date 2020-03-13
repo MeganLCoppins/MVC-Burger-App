a626a6cbb3fbe923f15d42b66d4549feb69ce897
Megan Coppins <meganlcoppins@gmail.com>
2020-03-12 20:05:49 -0700

    create routes commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,47 @@
-// 3. Inside the `burgers_controller.js` file, import the following:
+const express = require("express");
+const burger = require("../models/burger.js");
 
-//    * Express
-//    * `burger.js`
+const router = express.router();
 
-// 4. Create the `router` for the app, and export the `router` at the end of your file.
+// Create all the routes
+// get route
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+// post route
+router.post("/api/burgers", function(req, res) {
+  burger.addOne([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new one
+    res.json({ id: result.insertId });
+  });
+});
+// put route
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist = 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
