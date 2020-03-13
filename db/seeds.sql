504e7eec11a8a16ffedc4a7a14c79ba25823ec4b
Megan Coppins <meganlcoppins@gmail.com>
2020-03-12 17:27:07 -0700

    insert into burgers db commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
--- In this file, write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mac Burger', true);
