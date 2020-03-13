8f108db971ac71e48766e29b350bfcae2a45be22
Megan Coppins <meganlcoppins@gmail.com>
2020-03-12 19:45:52 -0700

    call orm functions commit

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,23 @@
-// * In `models`, make a `burger.js` file.
-
-// * Inside `burger.js`, import `orm.js` into `burger.js`
-
-// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+const orm = require("../config/orm.js");
+// call the ORM functions using burger specific input for the ORM.
+const burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+  };
 
 // * Export at the end of the `burger.js` file.
+module.exports = burger;
