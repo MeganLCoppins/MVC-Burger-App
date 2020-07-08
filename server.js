54914a94ac548b6b27b9d6d46d8d8bd7736e911e
Megan Coppins <meganlcoppins@gmail.com>
2020-07-08 13:48:02 -0700

    cleaned up, organized, and formatted code

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 // static content for app
 app.use(express.static("public"));
 // parse app body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // set handlebars
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}))
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
@@ -21,7 +21,7 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // start server so it begins listening to client requests
-app.listen(PORT, function(){
-    // log when server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-})
+app.listen(PORT, function () {
+  // log when server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
